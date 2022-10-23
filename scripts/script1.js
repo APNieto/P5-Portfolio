dffd422ac9a3eaeb3fc9f7b0215fffd59c46de21
APNieto <nietoalen@hotmail.com>
2022-10-23 17:54:02 -0300

    10/23.1 navbar, backtotop

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,15 +1,10 @@
-// function scrollToContact() {
-//     let contactSection = document.getElementById('_contact');
-//     contactSection.scrollIntoView({behavior:"smooth"});
-// }
-
 $(document).ready(function() {
     $("a").click(function(event) {
         $("html, body").animate({            
                 scrollTop: $($(this).attr("href")).offset().top
             }, 650);
         });
-    });
-
+    });   
 // Source for scrolling animation:
 // https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
+
