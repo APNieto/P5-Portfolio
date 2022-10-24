878c8743421a4ddcecd8bdeb662ca17b3d4d83dd
APNieto <nietoalen@hotmail.com>
2022-10-23 23:09:49 -0300

    10/23.2 navbar, colors

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-    $("a").click(function(event) {
-        $("html, body").animate({            
-                scrollTop: $($(this).attr("href")).offset().top
-            }, 650);
-        });
-    });   
+    let viewportWidth = window.innerWidth;
+    if (viewportWidth >= 992) {
+        $("a").click(function(event) {
+            $("html, body").animate({            
+                    scrollTop: $($(this).attr("href")).offset().top - 58
+                }, 650);
+            });           
+    } else {
+        $("a").click(function(event) {
+            $("html, body").animate({            
+                    scrollTop: $($(this).attr("href")).offset().top - 186
+                }, 650);
+            });           
+    }
+    
+});   
 // Source for scrolling animation:
 // https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
 
