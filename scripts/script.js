32313e4a6408b7c8a9170781cd32ee4654aba23d
APNieto <nietoalen@hotmail.com>
2023-01-24 19:03:37 -0300

    01/24.01 Reactive navbar, header padding

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
+// Elements
+const _navbar = document.getElementById('_navbar')
+const navbarBrand = document.getElementsByClassName('navbar-brand')[0]
+const navLink = document.getElementsByClassName('nav-link')
+
+
+
+// Event listeners
+
+window.onscroll = function(event) {
+    if(document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+        _navbar.style.padding = '0'
+        navbarBrand.style.fontSize = '1.4rem'        
+        for (let ele of navLink) {ele.style.fontSize = '1.2rem'}
+    } else {
+        _navbar.style.padding = '20px 0'
+        navbarBrand.style.fontSize = '1.8rem'
+        for (let ele of navLink) {ele.style.fontSize = '1.4rem'}
+    }
+    
+}
+
+
+
+
+
+// jQuery: Navbar buttons scroll to destination.
+// Source: https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
 $(document).ready(function() {
     let viewportWidth = window.innerWidth;
     if (viewportWidth >= 992) {
@@ -12,10 +40,5 @@ $(document).ready(function() {
                     scrollTop: $($(this).attr("href")).offset().top - 186
                 }, 180, "linear");
             });    
-    }
-
-
-    
+    }    
 });   
-// Source for scrolling animation:
-// https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
