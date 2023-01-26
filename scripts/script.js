c42717a0b398a62e17d010120d7091a004b791c3
APNieto <nietoalen@hotmail.com>
2023-01-26 12:06:24 -0300

    01/26.01 Portfolio projects animation

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,14 @@
 const _navbar = document.getElementById('_navbar')
 const navbarBrand = document.getElementsByClassName('navbar-brand')[0]
 const navLink = document.getElementsByClassName('nav-link')
-
+const projectsContainer = document.getElementById('_projects-container')
+const projectBoxes = document.getElementsByClassName('_project-boxes-invisible')
+const copyright = document.getElementById('_copyright')     //TEST
 
 
 // Event listeners
 
+// Diminishes/re-establishes navbar size on scroll
 window.onscroll = function(event) {
     if(document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
         _navbar.style.padding = '0'
@@ -16,11 +19,32 @@ window.onscroll = function(event) {
         _navbar.style.padding = '20px 0'
         navbarBrand.style.fontSize = '1.8rem'
         for (let ele of navLink) {ele.style.fontSize = '1.4rem'}
-    }
-    
+    }    
 }
 
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove('_project-boxes-invisible')
+            console.log('in');
+        } else {
+            entry.target.classList.add('_project-boxes-invisible')
+            console.log('out');
+        }})    
+}, {root: null, rootMargin: '0px', threshold: 1.0})
+for (let box of projectBoxes) {observer.observe(box)}
 
+
+//TEST
+// document.getElementById('test-button').onclick = function() {
+//     projectBoxes.forEach((element, index, array) => {
+//         array[index].classList.toggle('_project-boxes-invisible')
+//         console.log(array[index]);
+//     })
+//     // for (box of projectBoxes) {
+//     //     box.classList.toggle('_project-boxes-invisible')
+//     // }
+// }
 
 
 
