80ce11b22fd16829134d2998a7a4a12a6292db33
APNieto <nietoalen@hotmail.com>
2023-01-29 13:53:06 -0300

    01/29.01 Thumbnails added, scroll test

--- scripts/script.js
+++ scripts/script.js
@@ -149,19 +149,19 @@ function checkInputValidity() {
 
 // jQuery: Navbar buttons scroll to destination.
 // Source: https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
-$(document).ready(function() {
-    let viewportWidth = window.innerWidth;
-    if (viewportWidth >= 992) {
-        $("a").click(function(event) {
-            $("html, body").animate({            
-                    scrollTop: $($(this).attr("href")).offset().top - 58
-                }, 300);
-            });           
-    } else {
-        $("a").click(function(event) {
-            $("html, body").animate({            
-                    scrollTop: $($(this).attr("href")).offset().top - 186
-                }, 180, "linear");
-            });    
-    }    
-});   
+// $(document).ready(function() {
+//     let viewportWidth = window.innerWidth;
+//     if (viewportWidth >= 992) {
+//         $("a").click(function(event) {
+//             $("html, body").animate({            
+//                     scrollTop: $($(this).attr("href")).offset().top - 58
+//                 }, 300, "smooth");
+//             });           
+//     } else {
+//         $("a").click(function(event) {
+//             $("html, body").animate({            
+//                     scrollTop: $($(this).attr("href")).offset().top - 186
+//                 }, 180, "linear");
+//             });    
+//     }    
+// });   
